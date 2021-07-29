4f28c6e123d6afa41d3f2a66c4e0da7aec8512f6
chaewon <cw1205@duksung.ac.kr>
2021-07-29 17:46:35 +0900

    초급 Java_과제 수정
    1. CamelCase 사용
    2. 단계 줄이기
    3. System.exit 대신 return 사용하기
    4. 각각의 동작 Method로 만들기

--- Lotto.java
+++ Lotto.java
@@ -2,52 +2,74 @@ package jump2java;
 import java.util.Scanner;
 
 public class Lotto {
-
-	public static void main(String[] args) {
-		int[] autoNum = new int[6];
-		int[] lottoNum = new int[6];
-		String yn;
-		int count = 0;
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("로또 기계를 돌리시겠습니까?(y/n)");
-		yn = sc.next();
-		
-		for(int i = 0; i < lottoNum.length; i++) {
-			lottoNum[i] = (int)(Math.random()*45+1);
-		}
-		
-		if(yn.equals("y")) {
-			for(int i = 0; i < autoNum.length; i++) {
-				autoNum[i] = (int)(Math.random()*45+1);	
-				for(int k = 0; k <6; k++) {
-					for(int j = 0; j <6; j++) {
-						if(lottoNum[k] == autoNum[j]) {
-							count++;
-							}
-						}
-					}
-
-		}	System.out.print("[로또결과] 이채원님, 오늘은 ");
-			if(count==6) {
-				System.out.print("1등입니다!, ");
-			}else if(count==5) {
-				System.out.print("2등입니다!, ");
-			}else if(count==4) {
-				System.out.print("3등입니다!, ");
-			}else System.out.print("꽝입니다, ");
-			
-			System.out.print("뽑으신 번호 :");
-			for(int i = 0; i < autoNum.length; i++) {
-				System.out.print(" " + autoNum[i]);
-			}
-			
-		}else if(yn.equals("n")){
-			System.out.println("다음에 만나요.");
-			return;
-		}sc.close();
-	}
-		
-		
+	public static int[] lottoMachine = new int[6];
+	public static int[] lottoUser = new int[6];
+	public static int count;
+	public static String Name = new String();
+	public static Scanner sc = new Scanner(System.in);
 	
+	public static void main(String[] args) {
+		count = 0;
+		
+		// 이름 입력받기
+		inputName();
+		
+		// 기계가 뽑은 로또번호
+		makeLottoNum();
+		
+		// 사람이 뽑은 로또번호
+		makeUserNum();
+		
+		// 기계와 사람이 뽑은 번호 맞춰보기
+		makeCount();
+		
+		// 6개 - 1등, 5개 - 2등, 4개 - 3등, 나머지 꽝
+		informrRanking();
+
+	}
+
+	public static void informrRanking() {
+		System.out.print("[로또결과] " + Name + " 님, 오늘은 ");
+		if(count == 6) {
+			System.out.print("1등입니다!, ");
+		}else if(count == 5) {
+			System.out.print("2등입니다!, ");
+		}else if(count == 4) {
+			System.out.print("3등입니다!, ");
+		}else System.out.print("꽝입니다. ");
+		
+		System.out.print("뽑으신 번호 [");
+		for(int i = 0; i < 6; i++) {
+			System.out.print(" " + lottoUser[i]);
+		}
+		System.out.print(" ]");
+	}
+
+	public static void inputName() {
+		System.out.println("이름을 입력해주세요. ");
+		Name = sc.next();
+	}
+
+	public static void makeCount() {
+		for(int i = 0; i < 6; i++) {
+			int lottoUserNum = lottoUser[i];
+			int lottoMachineNum = lottoMachine[i];
+			if(lottoUserNum == lottoMachineNum) {
+				count += 1;
+			}
+		}
+	}
+
+	public static void makeUserNum() {
+		for(int i = 0; i < 6; i++) {
+			lottoUser[i] = (int)(Math.random() * 45 + 1);
+		}
+	}
+
+	public static void makeLottoNum() {
+		for(int i = 0; i < 6; i++) {
+			lottoMachine[i] = (int)(Math.random() * 45 + 1);
+		}
+	}
+
 }
